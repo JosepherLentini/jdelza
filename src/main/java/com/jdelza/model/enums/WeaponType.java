4668f364a1d351ad345f722989b7ec882f5baeda
unknown <lentini.giuseppe278@gmail.com>
2026-07-08 22:31:23 +0200

    feature: weapon structure

---
+++ src/main/java/com/jdelza/model/enums/WeaponType.java
@@ -0,0 +1,6 @@
+package com.jdelza.model.enums;
+
+public enum WeaponType {
+    SWORD, ROCK, SPEAR;
+
+}
--- src/main/java/com/jdelza/model/weapons/Weapon.java
+++ src/main/java/com/jdelza/model/weapons/Weapon.java
@@ -3,19 +3,42 @@ package com.jdelza.model.weapons;
 import com.jdelza.model.entities.Coordinates;
 import com.jdelza.model.entities.Entity;
 import com.jdelza.model.enums.DamageType;
+import com.jdelza.model.enums.WeaponType;
+import com.jdelza.model.interfaces.Damageable;
 import com.jdelza.model.interfaces.Damager;
 
-public abstract class Weapon extends Entity implements Damager {
+/**
+ * Weapon represent the type of weapon used by enemies or player
+ */
+public class Weapon extends Entity implements Damager {
 
     private DamageType attackPoints;
+    private WeaponType weapon;
 
-    public Weapon(Coordinates position, DamageType attackPoints) {
+    /**
+     * Constructor
+     * @param position      coordinates of weapon
+     * @param weapon        type of weapon
+     */
+    public Weapon(Coordinates position, WeaponType weapon) {
         super(position);
-        this.attackPoints = attackPoints;
+        this.weapon = weapon;
+        this.attackPoints = weapon == WeaponType.SWORD ? DamageType.NORMAL : DamageType.LIGHT;
+
     }
 
     //Get methods
     public DamageType getAttackPoints() {
         return attackPoints;
     }
+
+    @Override
+    public void toDamage(Damageable damageable) {
+
+        //When a collision occurs between sword and game characters, points are deducted
+        if (this.getPosition().equals(damageable.getPosition())){
+            damageable.takeDamage(this.attackPoints.getDamage());
+        }
+    }
+
 }
